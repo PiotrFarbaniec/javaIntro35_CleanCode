6c7b5ca3f029b7fff5a54833cdb7ebe2a6e7edb5
Piotr Farbaniec <piotr.farbaniec7@gmail.com>
2024-01-29 23:05:54 +0100

    Task-3--commit_03

--- src/main/java/task3/Calculator.java
+++ src/main/java/task3/Calculator.java
@@ -4,24 +4,24 @@ public class Calculator {
     public Calculator() {
     }
 
-    static Number getResultFor(char sign, Number valueA, Number valueB) {
+    public Number getResultFor(char sign, Number valueA, Number valueB) {
         Number result;
         if (sign == '+') {
-            result = new Calculator().summing.calculate(valueA, valueB);
+            result = this.summing.calculate(valueA, valueB);
         } else if (sign == '-') {
-            result = new Calculator().subtraction.calculate(valueA, valueB);
+            result = this.subtraction.calculate(valueA, valueB);
         } else if (sign == '*') {
-            result = new Calculator().multiplication.calculate(valueA, valueB);
+            result = this.multiplication.calculate(valueA, valueB);
         } else if (sign == '/') {
-            result = new Calculator().division.calculate(valueA, valueB);
+            result = this.division.calculate(valueA, valueB);
         } else {
             throw new IllegalArgumentException("Wrong operator: " + sign);
         }
         return result;
     }
 
-    Computable<Number> summing = (valA, valB) -> (valA.doubleValue() + valB.doubleValue());
-    Computable<Number> subtraction = (valA, valB) -> valA.doubleValue() - valB.doubleValue();
-    Computable<Number> division = (valA, valB) -> valA.doubleValue() / valB.doubleValue();
-    Computable<Number> multiplication = (valA, valB) -> valA.doubleValue() * valB.doubleValue();
+    private Computable<Number> summing = (valA, valB) -> (valA.doubleValue() + valB.doubleValue());
+    private Computable<Number> subtraction = (valA, valB) -> valA.doubleValue() - valB.doubleValue();
+    private Computable<Number> division = (valA, valB) -> valA.doubleValue() / valB.doubleValue();
+    private Computable<Number> multiplication = (valA, valB) -> valA.doubleValue() * valB.doubleValue();
 }
--- src/main/java/task3/Task3.java
+++ src/main/java/task3/Task3.java
@@ -4,10 +4,10 @@ public class Task3 {
 
     static int value1 = 10;
     static double value2 = 15.5d;
-
+    static Calculator cl = new Calculator();
     public static void main(String[] args) {
-        System.out.println(Calculator.getResultFor('+', value1, value2));
-        System.out.println(Calculator.getResultFor('-', value1, value2));
-        System.out.println(Calculator.getResultFor('/', value2, value1));
+        System.out.println(cl.getResultFor('+', value1, value2));
+        System.out.println(cl.getResultFor('-', value1, value2));
+        System.out.println(cl.getResultFor('/', value2, value1));
     }
 }
